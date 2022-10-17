5fefccf81d2c0844b2956e6785f6b68077f857d0
thierno953 <thiernobarry554@gmail.com>
2022-10-17 22:47:20 +0200

    redis

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+const session = require('express-session')
+const redis = require('redis')
+let RedisStore = require("connect-redis")(session)
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require('./config/config');
+
+let redisClient = redis.createClient({
+    host: REDIS_URL,
+    port: REDIS_PORT
+})
+
+
+const postRouter = require("./routes/postRoutes")
+const userRouter = require("./routes/userRoutes")
 
 const app = express();
 
@@ -23,11 +36,31 @@ const connectWithRetry = () => {
         })
 }
 
-connectWithRetry()
+connectWithRetry();
+
+app.enable("trust proxy") //this line tells express is behind a proxy.
+// app.use(cors({}))
+app.use(session({
+    store: new RedisStore({client: redisClient}),
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge:  30000, 
+    }
+}))
+
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("<h2>hi there!!</h2>")
-})
+    res.send("<h2>hi there!!!!</h2>")
+});
+
+app.use("/api/v1/posts", postRouter)
+app.use("/api/v1/users", userRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
